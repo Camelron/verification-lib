768d0449f1dcbdd466253c9252d2645ff6ad0404
cjen1-msft <chrisjensen@microsoft.com>
2025-12-30 17:12:50 +0000

    fmt

--- src/lib.rs
+++ src/lib.rs
@@ -4,8 +4,8 @@
 //! native service usage and for browser/WASM relying parties. For now it re-exports
 //! the `sev_verification` module which contains the verification engine.
 
-pub mod snp_report;
 pub mod crypto;
+pub mod snp_report;
 
 pub mod certificate_chain;
 
